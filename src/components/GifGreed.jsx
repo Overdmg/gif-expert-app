6c547f862f232cf2804eac07c2ce7c1addbca060
Overdmg <127998833+Overdmg@users.noreply.github.com>
2023-12-04 17:52:30 -0400

    Update GifGreed.jsx
    update

--- src/components/GifGreed.jsx
+++ src/components/GifGreed.jsx
@@ -1,12 +1,12 @@
 import { useState, useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
+
+
 export const GifGreed = ({ category }) => {
 
     const [images, setImages] = useState([]);
 
-    //console.log(images[6].title)
-    const listItems = images.map()
     
       const getImages = async () => {
             const newImages = await getGifs( category );
@@ -23,9 +23,11 @@ export const GifGreed = ({ category }) => {
             <>
                 <h3> { category } </h3>
                 <ol>
-                    {/* images.map ...*/}
-                   
-                    <li>Titulo:</li>
+                    {
+                        images.map (image => (
+                            <li key={ image.id }>{ image.title } </li>
+                        ))
+                    }                 
 
                 </ol>
             </>
