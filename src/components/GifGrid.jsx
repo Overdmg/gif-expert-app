b896d960cc5fc7bc6bdb3bb151eeb9be0af6519c
Overdmg <127998833+Overdmg@users.noreply.github.com>
2023-12-06 11:49:38 -0400

    update 2
    actualizacion al custom hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,24 +1,15 @@
 import { useState, useEffect } from "react";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 import { GifItem } from "./GifItem";
-//import { getGifs } from "../helpers/getGifs";
+
 
 export const GifGrid = ({ category }) => {
 
     const {images, isLoading} = useFetchGifs( category );
 
-    console.log({ images, isLoading});
+    //console.log({ images, isLoading});
 
-    // const [images, setImages] = useState([]);    
-    
-    //   const getImages = async () => {
-    //         const newImages = await getGifs( category );
-    //         setImages(newImages);
-    // }
 
-    // useEffect( () => { 
-    //     getImages();
-    // }, [])
 
     return (
             <>
